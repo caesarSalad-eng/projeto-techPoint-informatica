6ab577dcc73db905dc14cefe2a902c4f5a90f6c2
Kendall Ycaro <ycaroinacio203@gmail.com>
2025-12-05 14:40:17 -0300

    Primeiro commit do Projeto SpringBoot TechPoint Informatica

---
+++ projeto-techPoint-informatica/src/main/java/com/br/projeto_techPoint_informatica/ProjetoTechPointInformaticaApplication.java
@@ -0,0 +1,15 @@
+package com.br.projeto_techPoint_informatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTechPointInformaticaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjetoTechPointInformaticaApplication.class, args);
+
+	}
+
+}
---
+++ projeto-techPoint-informatica/src/test/java/com/br/projeto_techPoint_informatica/ProjetoTechPointInformaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projeto_techPoint_informatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTechPointInformaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
